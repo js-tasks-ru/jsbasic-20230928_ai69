d0c88e63baa25d28146c082610de914e99de3b05
artigor69 <ai69@mail.ru>
2023-10-03 22:25:51 +0400

    Модуль 2. Задание 1. Функция суммирования всех зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumOfSalary = 0;
+
+  for (let key in salaries) {
+    if (Number.isFinite(salaries[key])) {
+      sumOfSalary += salaries[key];
+    }
+  }
+
+  return sumOfSalary;
 }
