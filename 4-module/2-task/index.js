18f64b858d0ed26a16e6683c62f99b0c431b541c
artigor69 <ai69@mail.ru>
2023-10-15 19:38:19 +0400

    Модуль 4. Задание 2. Окрашивание фона диагонали таблицы в красный цвет.

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const changeColor = table.firstElementChild.children;
+
+  for (let i = 0; i < changeColor.length; i++) {
+    changeColor[i].children[i].style.backgroundColor = "red";
+  }
 }
