bf71d641db6294f09a5a9329863402f8d598b790
artigor69 <ai69@mail.ru>
2023-10-25 20:01:28 +0400

    Модуль 6. Задание 1. Создание таблицы с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,44 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.getTable();
+  }
+
+  getTable() {
+    let table = document.createElement("table");
+    let thead = document.createElement("thead");
+    thead.innerHTML =
+      "<tr><th>Имя</th>" +
+      "<th>Возраст</th>" +
+      "<th>Зарплата</th>" +
+      "<th>Город</th>" +
+      "<th></th></tr>";
+    table.append(thead);
+
+    let tbody = document.createElement("tbody");
+    table.append(tbody);
+
+    tbody.innerHTML = this.rows.reduce(
+      (acc, i) =>
+        acc +
+        (`<tr>` +
+          `<td>${i.name}</td>` +
+          `<td>${i.age}</td>` +
+          `<td>${i.salary}</td>` +
+          `<td>${i.city}</td>` +
+          `<td><button>X</button></td>
+          </tr>`),
+      ""
+    );
+
+    tbody.addEventListener("click", function (event) {
+      let button = event.target.closest("button");
+      if (!button) return;
+
+      button.closest("tr").remove();
+    });
+
+    return table;
   }
 }
